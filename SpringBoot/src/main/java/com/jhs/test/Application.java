3defcc24b0b2e669fb90a5fc3ef11c404c67d772
jhs666 <1283470165@qq.com>
2022-06-25 16:22:49 +0800

    修改了启动类，主要是注入的bean的名称错误

--- SpringBoot/src/main/java/com/jhs/test/Application.java
+++ SpringBoot/src/main/java/com/jhs/test/Application.java
@@ -2,13 +2,14 @@ package com.jhs.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext con = SpringApplication.run(Application.class, args);
-        Object obj = con.getBean("MyController");
+        Object obj = con.getBean("myController");
         System.out.println(obj);
     }
 
